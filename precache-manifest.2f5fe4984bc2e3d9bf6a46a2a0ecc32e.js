d47ad05af3422b9009b7dafa3df4e478c90b4344
kapil240605 <kapilagrawal612@gmail.com>
2024-03-01 09:35:26 +0530

    Updates

---
+++ precache-manifest.2f5fe4984bc2e3d9bf6a46a2a0ecc32e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c117682cbb164ff8ce49f0ef4723df",
+    "url": "/bootstrapreact/index.html"
+  },
+  {
+    "revision": "3f5863beb7bad6d21b3b",
+    "url": "/bootstrapreact/static/css/main.d2a6776a.chunk.css"
+  },
+  {
+    "revision": "b723b06a772ed3edf803",
+    "url": "/bootstrapreact/static/js/2.50d62705.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/bootstrapreact/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "3f5863beb7bad6d21b3b",
+    "url": "/bootstrapreact/static/js/main.6e5bce20.chunk.js"
+  },
+  {
+    "revision": "842b5e20a50c21ca2f4d",
+    "url": "/bootstrapreact/static/js/runtime~main.7238f1fb.js"
+  }
+]);
